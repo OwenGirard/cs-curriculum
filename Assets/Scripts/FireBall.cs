0f0b4dd66b517709ab796837d5907ee86a2c25bb
Owen G <144268700+OwenGirard@users.noreply.github.com>
2023-10-17 10:42:01 -0700

    nothing
    nothing

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    private GameObject Player;
+
+    public GameObject Fireball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //OnTriggerEnter2D()
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
