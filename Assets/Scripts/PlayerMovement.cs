27158f0fb0032412c421f2a4e85f58ca3718bb08
Owen G <144268700+OwenGirard@users.noreply.github.com>
2024-01-24 15:12:09 -0800

    2d

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,18 +13,20 @@ public class PlayerMovement : MonoBehaviour
     public int xSpeed;
     public int ySpeed;
     public float Jumppowa;
-    private Rigidbody m_Rigidbody;
+    private Rigidbody2D m_Rigidbody;
     private bool canjump;
+
+    public float raylength;
     // Start is called before the first frame update
     void Start()
     {
         if (inCaves)
         {
             ySpeed = 0;
-            Jumppowa = 1f;
+            Jumppowa = 200f;
         }
 
-        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Rigidbody = GetComponent<Rigidbody2D>();
     }
     
     
@@ -39,40 +41,60 @@ public class PlayerMovement : MonoBehaviour
         YVector = YDir * ySpeed * Time.deltaTime;
         transform.position = transform.position + new Vector3(XVector, 0f, 0f);
         transform.position = transform.position + new Vector3(0f, YVector, 0f);
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown("space") && canjump)
         {
+            Debug.Log("jumped");
             m_Rigidbody.AddForce(transform.up * Jumppowa); 
         }
-
-
-    }
-
-    private void FixedUpdate()
-    {
+        
         // Bit shift the index of the layer (8) to get a bit mask
-        int layerMask = 1 << 8;
+        //int layerMask = 1 << 8;
 
         // This would cast rays only against colliders in layer 8.
         // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-        layerMask = ~layerMask;
+        //layerMask = ~layerMask;
 
-        RaycastHit hit;
+        //RaycastHit hit;
+        
+        
         // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity,
-                layerMask))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance,
-                Color.yellow);
-            Debug.Log("Did Hit");
-            canjump = true;
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            Debug.Log("Did not Hit");
-            canjump = false;
+       //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), raylength))
+       // {
+      //      
+      //      Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down), Color.yellow);
+       //     //Debug.Log("Did Hit");
+        //    canjump = true;
+       // }
+        //else
+        //{
+       //     Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 10, Color.white);
+            //Debug.Log("Did not Hit");
+        //    canjump = false;
 
         }
+        
+        //Debug.Log(hit);
+        
+
     }
-}
+
+   // private void FixedUpdate()
+   // {
+     //   RaycastHit2D hit = Physics2D.Raycast(Transform.position, -Vector2.up);
+      //  if (hit.collider != null)
+    //    {
+     //       // Calculate the distance from the surface and the "error" relative
+      //      // to the floating height.
+      //      float distance = Mathf.Abs(hit.point.y - Transform.position.y);
+       //     float heightError = Jumppowa - distance;
+
+       //     // The force is proportional to the height error, but we remove a part of it
+            // according to the object's speed.
+       //     float force = liftForce * heightError - m_Rigidbody .velocity.y * damping;
+
+            // Apply the force to the rigidbody.
+        //    m_Rigidbody.AddForce(Vector3.up * force);
+       // }
+   // }
+//}
     
