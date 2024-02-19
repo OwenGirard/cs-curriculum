dd1c7fd9a0b06b4f83b895943c8e52da84c99899
Owen G <144268700+OwenGirard@users.noreply.github.com>
2024-02-19 11:00:15 -0800

    plat

--- Assets/Scripts/LeverPlat.cs
+++ Assets/Scripts/LeverPlat.cs
@@ -1,34 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.SpriteAssetUtilities;
 using UnityEngine;
 
 public class leverPlat : MonoBehaviour
 {
-    public Transform[] waypoints;
+    
     private Lever script;
 
     private float speed;
-
+    private bool forward = true;
     private int currentWaypointIndex = 0;
-    public Vector3[] positions; 
+    public Vector2[] positions; 
     // Start is called before the first frame update
     void Start()
     {
         speed = 5f;
-        transform.position = positions[0];
+        transform.position = positions[currentWaypointIndex];
         script = GameObject.FindObjectOfType<Lever>();
-        if (waypoints.Length > 0)
-        {
-            //positions = waypoints[currentWaypointIndex].position;
-        }
+       
+        
     }
 
     // Update is called once per frame
+    private void OnCollisionEnter(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            other.gameObject.transform.SetParent(null);
+        }
+    }
+
     void Update()
     {
         if (script.on)
         {
-            //move
+            transform.position = Vector2.MoveTowards(transform.position, positions[currentWaypointIndex],
+                speed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, positions[currentWaypointIndex]) < 0.5f)
+            {
+                if (forward)
+                {
+                    if (currentWaypointIndex != positions.Length - 1)
+                    {
+                        currentWaypointIndex++;
+                    }
+                    else
+                    {
+                        forward = false;
+                    }
+                }
+                else
+                {
+                    if (currentWaypointIndex != 0)
+                    {
+                        currentWaypointIndex--;
+                    }
+                    else
+                    {
+                        forward = true;
+                    }
+                }
+            }
         }
     }
 }
